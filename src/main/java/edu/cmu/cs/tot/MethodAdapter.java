e3f0624915aebee7b9fb137d694d927c06f36ed7
Miguel Velez <miguelvelez@mijecu25.com>
2018-10-15 19:46:42 -0400

    removed extra ()

--- src/main/java/edu/cmu/cs/tot/MethodAdapter.java
+++ src/main/java/edu/cmu/cs/tot/MethodAdapter.java
@@ -16,14 +16,15 @@ class MethodAdapter extends MethodVisitor implements Opcodes {
   public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
     // notes: all names are fully qualified. fully qualified names use slashes internally instead of
     // dots.
-    if (("java/lang/Integer".equals(owner)) && ("intValue".equals(name))) {
+    if ("java/lang/Integer".equals(owner) && "intValue".equals(name)) {
       System.err.println("instrumenting call to intValue");
       // all operations are listed in Opcodes - those match the names of byte code instructions
       // to generate a bytecode instruction find the corresponding visit method (read Javadoc
       // documentation)
       this.visitInsn(Opcodes.DUP);
       // description is in an internal format that is described in the JVM specification
-      this.visitMethodInsn(Opcodes.INVOKESTATIC, "edu/cmu/cs/tot/Library", "foo", "(Ljava/lang/Integer;)V", false);
+      this.visitMethodInsn(
+          Opcodes.INVOKESTATIC, "edu/cmu/cs/tot/Library", "foo", "(Ljava/lang/Integer;)V", false);
     }
 
     super.visitMethodInsn(opcode, owner, name, desc, itf);
